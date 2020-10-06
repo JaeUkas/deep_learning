eefc4508456ad3e89d660417005166d0961199c3
JaeUkas <anytime96@naver.com>
2020-10-06 17:35:24 +0900

    학습데이터 다운로드

---
+++ download_data.py
@@ -0,0 +1,12 @@
+import os
+from six.moves import urllib
+
+def download_data(url, dir_name, file_name):
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+    file_path = os.path.join(dir_name, file_name)
+    urllib.request.urlretrieve(url, file_path)
+
+dir_name = "datasets/iris"
+file_name = "iris.csv"
+download_data("https://www.openml.org/data/get_csv/61/datasets_61_iris.arft", dir_name, file_name)
