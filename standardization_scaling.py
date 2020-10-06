dabde8f5b2b9d2a60bba71dafddcd053746ea728
JaeUkas <anytime96@naver.com>
2020-10-06 21:46:25 +0900

    데이터 스케일링

--- standardization_scaling.py
+++ standardization_scaling.py
@@ -11,7 +11,10 @@ iris_train_data, iris_test_data, iris_train_label, iris_test_label = train_test_
 scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
 scaler.fit(iris_train_data)
 scaled_iris_train_data = scaler.transform(iris_train_data)
+
 scaled_iris_test_data = scaler.transform(iris_test_data)
+#fit을 한 번 더 적용하면 학습 데이터와 테스트 데이터 간 스케일링이 맞지않아진다.
+#ML 모델은 학습 데이터를 기반으로 학습되기때문에 반드시 테스트 데이터는 학습 데이터의 스케일링 기준을 따라야한다.
 
 print("학습 데이터", " : \n", scaled_iris_train_data)
 print("테스트 데이터", " : \n", scaled_iris_test_data)
